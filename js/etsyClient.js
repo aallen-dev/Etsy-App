55958ca1f392f6d9b2089131fe20d4b0faca1060
Alexander Allen <aallen@blazinweb.com>
2015-02-10 18:02:01 -0600

    quicker response with touchstart

--- js/etsyClient.js
+++ js/etsyClient.js
@@ -368,7 +368,7 @@
                     $('#banner-' + node.attr('listing'))
                         .off()
                         .on(self.touchstartEventName ,function(event) {
-                            alert(self.touchstartEventName+'s')
+                            alert(self.touchstartEventName+'d')
                             if (!this.parentNode.parentNode.className.match('showDescription'))//||
                                 return
 
